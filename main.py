6cee17def90f9a648ecfdaeaf0a5b978ae753f77
Shivam_Singh <shivamsingh6490@gmail.com>
2024-03-29 18:29:51 +0530

    Designing front end website

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('login.html')
+
+@app.route('/signup',methos=['GET','POST'])
+def sign():
+    if request.method =='POST':
+        #handle request
+        pass
+    return render_template('signup.html')
+
+@app.route('/home')
+def home():
+    return render_template('dashboard.html')
 
 if __name__=="__main__":
     app.run(debug=True)
