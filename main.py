94a68eb8ed5ec415d086ad470ebe16d21b808283
Shivam_Singh <shivamsingh6490@gmail.com>
2024-04-06 14:25:45 +0530

    stegnography added in this project

--- main.py
+++ main.py
@@ -6,14 +6,12 @@ from datetime import datetime
 import os
 from flask import send_from_directory
 from flask import send_file
+from steganography import create_image, decode_image
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    if request.method =='POST':
-        #handle request
-        pass
+def login():
     return render_template('login.html')
 
 def encrypt(file):
@@ -48,7 +46,7 @@ def decrypt(key,file):
 
 
 @app.route('/home')
-def home():
+def index():
     """Renders the home page."""
     return render_template(
         'index.html',
@@ -118,7 +116,68 @@ def sign():
     return render_template('signup.html')
 
 
+## stegnography code
+UPLOAD_FOLDER = os.path.join('static/images')
+ALLOWED_EXTENSIONS = {'png'}
 
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+if os.path.exists(app.config['UPLOAD_FOLDER']) is False:
+    os.mkdir(app.config['UPLOAD_FOLDER'])
+
+
+@app.route('/stegnography')
+def home():
+    return render_template('stegno.html')
+
+
+@app.route('/encode', methods=['GET', 'POST'])
+def encode():
+    if request.method == 'POST':
+
+        # LOADING AND SAVING IMAGE
+        if 'image' not in request.files:
+            return 'there is no image in form!'
+        image = request.files['image']
+        path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
+
+        # delete cached file
+        if os.path.isfile(path):
+            os.remove(path)
+
+        image.save(path)
+
+        # STEGANOGRAPHY
+
+        secret_message = request.form['secret_message']
+
+        base_image_path = path
+        create_image(secret_message, base_image_path)
+
+        return render_template('result_encode.html', img_path=base_image_path)
+
+    return render_template('encode.html')
+
+
+@app.route('/decode', methods=['GET', 'POST'])
+def decode():
+
+    if request.method == 'POST':
+
+        # LOADING AND SAVING IMAGE
+        if 'image' not in request.files:
+            return 'there is no image in form!'
+        image = request.files['image']
+        path = os.path.join(app.config['UPLOAD_FOLDER'], image.filename)
+        image.save(path)
+
+        # STEGANOGRAPHY
+
+        secret = decode_image(path)
+        return render_template('result_decode.html', secret=secret)
+
+    return render_template('decode.html')
 
 if __name__=="__main__":
     app.run(debug=True)
